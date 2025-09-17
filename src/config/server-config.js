7a2b8dcac5e1fee35d83a1c1c0017b4189bdd6b0
Ashish_Jain <121597042+asj0000@users.noreply.github.com>
2025-09-17 13:49:35 +0530

    added rabbit mq setup

--- src/config/server-config.js
+++ src/config/server-config.js
@@ -5,5 +5,8 @@ dotenv.config();
 module.exports = {
     PORT: process.env.PORT,
     EMAIL_ID: process.env.SMTP_MAIL_ID,
-    PASSWORD: process.env.SMTP_PWD
+    PASSWORD: process.env.SMTP_PWD,
+    MESSAGE_BROKER_URL:process.env.MESSAGE_BROKER_URL,
+    EXCHANGE_NAME: process.env.EXCHANGE_NAME,
+    REMINDER_BINDING_KEY: process.env.REMINDER_BINDING_KEY,
 }
---
+++ src/utitls/messageQueue.js
@@ -0,0 +1,46 @@
+const amqplib = require('amqplib')
+const { MESSAGE_BROKER_URL , EXCHANGE_NAME } = require('../config/server-config');
+
+
+const createChannel = async()=>{
+    try {
+        const connection = await amqplib.connect(MESSAGE_BROKER_URL);
+        const channel =    await connection.createChannel();
+        channel.assertExchange(EXCHANGE_NAME  , 'direct' , false);
+        return channel ;
+    } catch (error) {
+        console.log("Error in message queue" , error);
+        throw error;        
+    }
+
+}
+
+const subscribeMessage  = async (channel , service , bindingKey)=>{
+    try {
+        const applicationQueue  = await channel.assertQueue(bindingKey);
+        channel.bindQueue(applicationQueue.queue , EXCHANGE_NAME , bindingKey);
+        channel.consume(applicationQueue.queue , msg=>{
+            console.log("Received data");
+            console.log(msg.content.toString());
+            channel.ack(msg);
+        })
+
+    } catch (error) {
+        throw error;
+    }
+
+
+}
+
+const publishMessage = async(channel ,bindingKey, message)=>{
+    try {
+        await channel.assertQueue(bindingKey);
+        await channel.publish( EXCHANGE_NAME , bindingKey , Buffer.from(message) );
+    } catch (error) {
+        throw error;     
+        
+    }
+
+}
+
+module.exports = {createChannel , subscribeMessage, publishMessage}
